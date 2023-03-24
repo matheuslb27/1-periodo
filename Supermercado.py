833f509de35728039cfbc38a9e099dff96f36785
Matheus Lopes <127918380+matheuslb27@users.noreply.github.com>
2023-03-23 21:02:52 -0300

    Update Supermercado.py

--- Supermercado.py
+++ Supermercado.py
@@ -3,9 +3,9 @@ import time
 print('Bem vindo ao Supermercado online!')
 itens=['Maçã','Uva','Banana']
 itens_lista=[]
-preços=[2.99,1.99,4.99]
-soma=0
+preço=[2.99,1.99,4.99]
 time.sleep(2)
+
 print('''
 itens disponíveis: 
 ---------------''')
@@ -15,37 +15,36 @@ print('---------------')
 time.sleep(1.3)
 
 while True:
-    item = input("Digite o item que você quer adicionar ao carrinho: ")
-    if item in itens:
-        itens_lista.append(item)
-        itens.remove(item)
-        print('Seu carrinho: ')
-        print('-'.join(itens_lista))
-    else:
+    item = input("Digite o item que você quer adicionar ao carrinho : ")
+    if item not in itens:
         print('Esse item não existe')
-
+        continue
+    itens_lista.append(item)
     parar=input('Deseja adicionar mais itens? ')
-    if parar=='n' or parar=='nao' or parar== 'não':
+    if parar in ['n', 'nao', 'não']:
         break
+    
+print('Seu carrinho: ')
+for item in itens_lista:
+    print("- " + item)
 
 while True:
     remover = input("deseja remover algum item? ")
-    if remover== 's' or remover=='sim':
+    if remover in ['s', 'sim']:
         remover_item=input('Qual item você deseja remover? ')
         if remover_item in itens_lista:
             itens_lista.remove(remover_item)
-            print('Seu carrinho: ')
-            print('-'.join(itens_lista))
         else:
-            print('Este item não está no carrinho.')
+            print('Esse item não está no carrinho')
+        print("Seu carrinho: ")
+        print("\n".join("- " + item for item in itens_lista))
+
     else:
         time.sleep(1.3)
         print('Carrinho salvo')
         break
 
-time.sleep(1.3)
-total = sum([preços[itens.index(item)] for item in itens_lista])
-print("Seu carrinho:")
+total = sum(preço[itens.index(item)] for item in itens_lista)
 for item in itens_lista:
     print("- " + item)
 print("Total da compra: R$ %.2f" % total)
